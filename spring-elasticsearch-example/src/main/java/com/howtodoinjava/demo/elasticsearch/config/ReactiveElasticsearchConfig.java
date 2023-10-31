b880ef88a83ff1e946e238877b5a3848de1981aa
Bayvao <vermabayvao@gmail.com>
2023-10-31 13:14:03 +0530

    code for Spring Data with Elasticsearch

---
+++ spring-elasticsearch-example/src/main/java/com/howtodoinjava/demo/elasticsearch/config/ReactiveElasticsearchConfig.java
@@ -0,0 +1,14 @@
+package com.howtodoinjava.demo.elasticsearch.config;
+
+import org.springframework.data.elasticsearch.client.ClientConfiguration;
+import org.springframework.data.elasticsearch.client.elc.ReactiveElasticsearchConfiguration;
+
+//@Configuration
+public class ReactiveElasticsearchConfig extends ReactiveElasticsearchConfiguration {
+    @Override
+    public ClientConfiguration clientConfiguration() {
+        return ClientConfiguration.builder()
+                .connectedTo("localhost:9200")
+                .build();
+    }
+}
